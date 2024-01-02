5cc8996b94ff22cf8e9a75e99cf9bb6cf4a895d6
param1989 <42086618+param1989@users.noreply.github.com>
2024-01-02 14:11:01 +0530

    Update 02_data_types.js

--- JS-Training/02_data_types.js
+++ JS-Training/02_data_types.js
@@ -1,1 +1,30 @@
 
+"use strict" //treat all the JS Code as newer version
+let name = "Paramjit" //it will treated as a string
+let age = 34 //it will be treated as a number
+let isLoggedIn = false //it will be treated as a boolean where true means 1 and false means 0
+let state = null; //empty value
+
+console.log(typeof(name));
+//type of function indicates the data type of a variable
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(state));
+/*
+if u haven't assigned any value it will be treated as an undefined
+*/
+/*
+Primitive Date Types
+number
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => unique -> used in react.js
+*/
+
+//object is a user defined data type
+console.log(typeof(undefined)) //undefined
+console.log(typeof(null)) //object
+
