f733c2c8647e7a19ab9bc43152d28f5561fc390e
param1989 <42086618+param1989@users.noreply.github.com>
2024-01-11 15:21:33 +0000

    commit

--- JS-Training/08_arrays.js
+++ JS-Training/08_arrays.js
@@ -35,6 +35,7 @@ const allArr = myArr.concat(myArr2)
 console.log(allArr)
 
 
+    
 
 /*
 The slice() method can be used to create a copy of an array or return a portion of an array. It is important to note that the slice() method does not alter the original array but instead creates a shallow copy. Unlike the slice() method, the splice() method will change the contents of the original array
